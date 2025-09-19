ae1e8c2924dfddd1403be35a26758eb3a7a8b6f3
Edithson <moafogaus@gmail.com>
2025-09-19 16:40:15 +0100

    Feature : class views's applications

--- pages/urls.py
+++ pages/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home_page_view),
-    path("contact/", views.contact_page_view),
+    path("", views.HomePageView.as_view()),
+    # path("", views.home_page_view),
+    path("contact/", views.MessageListView.as_view()),
+    # path("contact/", views.contact_page_view),
     path("contact/create/", views.create_contact_page_view, name="create_contact"),
     path("info/", views.info_page_view)
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,23 +4,44 @@ from django.http import HttpResponse
 
 from pages.models import ContactMessage
 from .forms import ContactForms
+from django.views.generic import TemplateView
+from django.views.generic import ListView
 
-def home_page_view(request):
-    context = {
-        'nom' : 'John',
-        'age' : 21,
-        'taches' : ['métro', 'boulo', 'dodo'],
-        'genre' : 'M',
-    }
-    return render(request, "home.html", context)
+# def home_page_view(request):
+#     context = {
+#         'nom' : 'John',
+#         'age' : 21,
+#         'taches' : ['métro', 'boulo', 'dodo'],
+#         'genre' : 'M',
+#     }
+#     return render(request, "home.html", context)
 
-def contact_page_view(request):
-    contacts = ContactMessage.objects.all()
-    return render(
-            request,
-            "contact/list.html",
-            context={'contacts' : contacts}
-        )
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["nom"] = 'John Doe'
+        context["age"] = 21
+        context["taches"] = ['métro', 'boulo', 'dodo']
+        context["genre"] = 'F'
+        return context
+    
+# def contact_page_view(request):
+#     contacts = ContactMessage.objects.all()
+#     return render(
+#             request,
+#             "contact/list.html",
+#             context={'contacts' : contacts}
+#         )
+    
+class MessageListView(ListView):
+    model = ContactMessage
+    template_name = 'contact/list.html'
+    context_object_name = 'contacts'
+    
+    def get_queryset(self):
+        return ContactMessage.objects.filter(is_treated=False)
+    
 
 def create_contact_page_view(request):
     success_msg = None
@@ -28,7 +49,7 @@ def create_contact_page_view(request):
         form = ContactForms(request.POST)
         if form.is_valid():
             form.save()
-            form = ContactForms() #on nétoie le formulaire
+            form = ContactForms() #on vide le formulaire
             success_msg = "Votre demande a bien été envoyée"
     else :
         form = ContactForms()
